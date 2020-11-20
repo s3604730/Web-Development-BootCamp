6823045edfe0b0323fa27d756325db81a8e2f944
Tony Tran <tonytranme0@hotmail.com>
2020-11-20 17:04:53 +1100

    Adding Section32 files

--- Secrets+-+Starting+Code/Secrets - Starting Code/app.js
+++ Secrets+-+Starting+Code/Secrets - Starting Code/app.js
@@ -12,6 +12,17 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+app.get("/register", function(req, res) {
+    res.render("register");
+});
 
 
 app.listen(3000, function() {
